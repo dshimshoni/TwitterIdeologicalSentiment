19623b15768ef11bd384365e678ab1763cdd9567
Daniel Richardson <drichardson005@gmail.com>
2021-03-10 19:00:30 -0500

    Update

--- archive_search.py
+++ archive_search.py
@@ -22,26 +22,56 @@ auth.set_access_token(_access_token, _access_token_secret)
 api = tweepy.API(auth)
 
 
-#Give 10 tweets that mention socialism from February 1 2020 to March 1 2020
-results = api.search_full_archive('development',
-                                  'socialism',
-                                  fromDate = '202002010000',
-                                  toDate = '202002010010',
-                                  maxResults = 1)
+#Give 500 tweets that mention socialism from February 1 2020 to March 1 2020
+# results = api.search_full_archive('development',
+#                                   'socialism',
+#                                   fromDate = '202002010000',
+#                                   toDate = '202002010010',
+#                                   maxResults = 1)
 
 total = 1
 
-for tweet in results:
-    if(tweet.lang == 'en'):
+# for tweet in results:
+#     if(tweet.lang == 'en'):
         
-        #Maybe is:retweet works here??
-        if(not re.search('RT +.', tweet.text)):
+#         #Maybe is:retweet works here??
+#         if(not re.search('RT +.', tweet.text)):
             
-            #Didn't test this yet, may eliminate too many tweets.
-            # if('USA' in tweet.user.location):
-                print(total,tweet.text,tweet.user.location)
-                total += 1
+#             #Didn't test this yet, may eliminate too many tweets.
+#             # if('USA' in tweet.user.location):
+#                 print(total,tweet.text,tweet.user.location)
+#                 total += 1
             
 print(total - 1)
+    
+for i in range(28):    
+    day1 = i+1
+    day2 = i+2
+    if(i < 8):
+        print('2020020'+str(day1)+'00','2020020'+str(day2)+'00')
+        # results = api.search_full_archive('development',
+        #                       'socialism',
+        #                       fromDate = '2020020'+str(day1)+'00',
+        #                       toDate = '2020020'+str(day2)+'00',
+        #                       maxResults = 1)
+    elif(i == 9):
+        print('2020020'+str(day1)+'00','202002'+str(day2)+'00')
+        # results = api.search_full_archive('development',
+        #               'socialism',
+        #               fromDate = '2020020'+str(day1)+'00',
+        #               toDate = '202002'+str(day2)+'00',
+        #               maxResults = 1)
+    else:
+        print('202002'+str(day1)+'00','202002'+str(day2)+'00')
+        # results = api.search_full_archive('development',
+        #               'socialism',
+        #               fromDate = '202002'+str(day1)+'00',
+        #               toDate = '202002'+str(day2)+'00',
+        #               maxResults = 1)
+    
+    
+    
+    
+    
     
     
