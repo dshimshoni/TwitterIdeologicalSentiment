7fac8143383f4e2ee0630f15d15f1c9f34a3c0ba
Daniel Richardson <drichardson005@gmail.com>
2021-03-16 12:46:06 -0400

    Added another column that is each cleaned tweet as a string, simplifying the sentiment analyses.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -14,14 +14,10 @@ def afinn_sentiment(corpus):
     af = Afinn()
     af_scores = []
     
-    for token_list in corpus['tokens']:
-        total = 0
-        for token in token_list:
-            total += af.score(token)
-        af_scores.append(total)
+    for tweet in corpus['cleaned_tweets']:
+        af_scores.append(af.score(tweet))
 
     return af_scores
-
 
 def vader_sentiment(corpus):
     # You'll need to run this if you've never downloaded the 
@@ -33,36 +29,45 @@ def vader_sentiment(corpus):
     
     sid = SentimentIntensityAnalyzer()
     
-    for token_list in corpus['tokens']:
-        total = 0
-        for token in token_list:
-            total += sid.polarity_scores(token)['compound']
-        compounds.append(total)
+    # for token_list in corpus['tokens']:
+    #     score = 0
+    #     cleaned_tweet = ''
+    #     for token in token_list:
+    #         cleaned_tweet += token + ' '
+    #     print(cleaned_tweet)
+    #     score = sid.polarity_scores(cleaned_tweet)['compound']
+        # compounds.append(score)
+    
+    for tweet in corpus['cleaned_tweets']:
+        compounds.append(sid.polarity_scores(tweet)['compound'])
     
     return compounds
 
 
 df = pd.read_csv('senator_tweets.csv')
 
-#Add tokens as a df column
-
 tk = WhitespaceTokenizer()
 tokens = []
+cleaned_tweets = []
 for tweet in df['text']:
     tweet_tokens = []
+    cleaned_tweet = ''
     for token in tk.tokenize(tweet):
         if('@' not in token and 'http' not in token):
             #If we wanted this is where stop word removal
             #and other cleaning could happen
             
             tweet_tokens.append(token)
+            cleaned_tweet += token + ' '
     tokens.append(tweet_tokens)
+    cleaned_tweets.append(cleaned_tweet)
             
 df['tokens'] = tokens
-df['af_scores'] = afinn_sentiment(df)
+df['cleaned_tweets'] = cleaned_tweets
+df['afinn_scores'] = afinn_sentiment(df)
 df['vader_scores'] = vader_sentiment(df)
 
 df.to_csv('data_with_scores.csv', index=False)
     
-    
+
     
