12b3f030fafc857b42cb2b548d0787a49887d884
Daniel Richardson <drichardson005@gmail.com>
2021-03-16 12:19:46 -0400

    Converted the data into a dataframe with all of the fields from the original csv, plus a cleaned token column and two sentiment score columns.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,66 +6,63 @@ Created on Wed Mar 10 21:02:56 2021
 @author: danielrichardson
 """
 
-import nltk
-import csv
+from nltk.tokenize import WhitespaceTokenizer
+import pandas as pd
 from afinn import Afinn
-import matplotlib.pyplot as plt
 
 def afinn_sentiment(corpus):
-    total = 0
-
-    for i in range(len(corpus)):
-        total += af.score(corpus[i])
-        
+    af = Afinn()
+    af_scores = []
     
-    return total/len(corpus)
+    for token_list in corpus['tokens']:
+        total = 0
+        for token in token_list:
+            total += af.score(token)
+        af_scores.append(total)
+
+    return af_scores
 
 
 def vader_sentiment(corpus):
+    # You'll need to run this if you've never downloaded the 
+    # vader lexicon before
     # nltk.download('vader_lexicon')
+    
     compounds = []
     from nltk.sentiment.vader import SentimentIntensityAnalyzer
     
     sid = SentimentIntensityAnalyzer()
     
-    total = 0
-    for tweet in corpus:
-        total += sid.polarity_scores(tweet)['compound']
-        compounds.append(sid.polarity_scores(tweet)['compound'])
+    for token_list in corpus['tokens']:
+        total = 0
+        for token in token_list:
+            total += sid.polarity_scores(token)['compound']
+        compounds.append(total)
+    
+    return compounds
+
+
+df = pd.read_csv('senator_tweets.csv')
+
+#Add tokens as a df column
+
+tk = WhitespaceTokenizer()
+tokens = []
+for tweet in df['text']:
+    tweet_tokens = []
+    for token in tk.tokenize(tweet):
+        if('@' not in token and 'http' not in token):
+            #If we wanted this is where stop word removal
+            #and other cleaning could happen
+            
+            tweet_tokens.append(token)
+    tokens.append(tweet_tokens)
+            
+df['tokens'] = tokens
+df['af_scores'] = afinn_sentiment(df)
+df['vader_scores'] = vader_sentiment(df)
+
+df.to_csv('data_with_scores.csv', index=False)
     
     
-
-    # An "interface" to matplotlib.axes.Axes.hist() method
-    n, bins, patches = plt.hist(x=compounds, bins='auto', color='#0504aa',
-                                alpha=0.7, rwidth=0.85)
-    plt.grid(axis='y', alpha=0.75)
-    plt.xlabel('Vader Compound Sentiment Score')
-    plt.ylabel('Frequency')
-    plt.title('Tweets about Kirsten Synema up to 3/5')
-    # plt.text(23, 45, r'$\mu=15, b=3$')
-    plt.show()
-    
-    return total/len(corpus)
-
-
-
-af = Afinn()
-
-sinema_before_corpus = []
-
-with open('sinemabeforetweets.csv') as file:
-    csv_reader = csv.reader(file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count > 0:
-           sinema_before_corpus.append(row[3])
-        line_count += 1
-
-# tokenizer = nltk.tokenize.WhitespaceTokenizer()
-
-
-print(afinn_sentiment(sinema_before_corpus))
-print(vader_sentiment(sinema_before_corpus))
-
-
     
